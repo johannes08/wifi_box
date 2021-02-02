fc900005d851586e6a5d0e4897723d4cc5a8b269
johannes <johhuegel@gmail.com>
2021-02-02 17:54:28 +0100

    high edge for 5 seconds

---
+++ secret.h
@@ -0,0 +1,2 @@
+#define SECRET_SSID "wjhsurface"
+#define SECRET_PASS "gggggggg"
--- wifi_box.ino
+++ wifi_box.ino
@@ -1,84 +1,133 @@
 #include <SPI.h>
 #include <WiFiNINA.h>
 
-#define led 13
+#include "secret.h"
 
-char ssid[] = "Internet27";
-char pass[] = "97975411637015086953";
-int keyIndex = 0;
+char ssid[] = SECRET_SSID;        // your network SSID (name)
+char pass[] = SECRET_PASS;    // your network password (use for WPA, or use as key for WEP)
+int keyIndex = 0;                 // your network key index number (needed only for WEP)
 
 int status = WL_IDLE_STATUS;
 WiFiServer server(80);
 
-String readString;
-
-// Send status to server
-// Set Wifi Data dynamically
-
+int lastOpenTime = 0;
+bool activeOutput = false;
 
 void setup() {
-  pinMode(led, OUTPUT);
-  Serial.begin(9600);
+    Serial.begin(9600);      // initialize serial communication
+    pinMode(4, OUTPUT);      // set the LED pin mode
 
-  while (status != WL_CONNECTED) {
-    Serial.print("Attempting to connect to Network named: ");
-    Serial.println(ssid);
-    status = WiFi.begin(ssid, pass);
-    delay(10000);
-  }
-  server.begin();
+    // check for the WiFi module:
+    if (WiFi.status() == WL_NO_MODULE) {
+        Serial.println("Communication with WiFi module failed!");
+        // don't continue
+        while (true);
+    }
 
-  Serial.print("SSID: ");
-  Serial.println(WiFi.SSID());
-  IPAddress ip = WiFi.localIP();
-  Serial.print("IP Address: ");
-  Serial.println(ip);
+    String fv = WiFi.firmwareVersion();
+    if (fv < WIFI_FIRMWARE_LATEST_VERSION) {
+        Serial.println("Please upgrade the firmware");
+    }
+
+    // attempt to connect to WiFi network:
+    while (status != WL_CONNECTED) {
+        Serial.print("Attempting to connect to Network named: ");
+        Serial.println(ssid);                   // print the network name (SSID);
+
+        // Connect to WPA/WPA2 network. Change this line if using open or WEP network:
+        status = WiFi.begin(ssid, pass);
+        // wait 10 seconds for connection:
+        delay(10000);
+    }
+    server.begin();                           // start the web server on port 80
+    printWifiStatus();                        // you're connected now, so print out the status
 }
 
+
 void loop() {
-  WiFiClient client = server.available();
-  if (client)
-  {
-    Serial.println("new client");
-
-    while (client.connected())
-    {
-      if (client.available())
-      {
-        char c = client.read();
-        if (readString.length() < 100)
-        {
-          readString += c;
-          Serial.write(c);
-          
-          if (c == '\n') {
-            client.println("<a href=\"/?1\"\">Turn On Light</a>");
-            client.println("<br />");
-            client.println("<br />");
-            client.println("<a href=\"/?0\"\">Turn Off Light</a><br />");     
-
-            delay(1);
-            
-            if(readString.indexOf("?1") > 0)
-            {
-              digitalWrite(led, HIGH);
-              delay(1);
-            }
-            else{
-              if(readString.indexOf("?0") > 0)
-              {
-                digitalWrite(led, LOW);    
-                delay(1);
-              }
-            }           
-            readString="";
-
-            delay(1);
-            client.stop();
-            Serial.println("client disonnected");
-          }
-        }
-      }
+    if(activeOutput && lastOpenTime + 5000 < millis()){
+        switchOff();
+        Serial.println("switchOff");
     }
-  }
+
+    WiFiClient client = server.available();   // listen for incoming clients
+
+    if (client) {                             // if you get a client,
+        Serial.println("new client");           // print a message out the serial port
+        String currentLine = "";                // make a String to hold incoming data from the client
+        while (client.connected()) {            // loop while the client's connected
+            if (client.available()) {             // if there's bytes to read from the client,
+                char c = client.read();             // read a byte, then
+                Serial.write(c);                    // print it out the serial monitor
+                if (c == '\n') {                    // if the byte is a newline character
+
+
+                    // if the current line is blank, you got two newline characters in a row.
+                    // that's the end of the client HTTP request, so send a response:
+                    if (currentLine.length() == 0) {
+                        // HTTP headers always start with a response code (e.g. HTTP/1.1 200 OK)
+                        // and a content-type so the client knows what's coming, then a blank line:
+                        client.println("HTTP/1.1 200 OK");
+                        client.println("Content-type:text/html");
+                        client.println();
+
+                        // the content of the HTTP response follows the header:
+                        client.print("Click <a href=\"/H\">here</a> turn the LED on pin 4 on<br>");
+                        client.print("Click <a href=\"/L\">here</a> turn the LED on pin 4 off<br>");
+
+                        // The HTTP response ends with another blank line:
+                        client.println();
+
+                        Serial.println(currentLine);
+                        // break out of the while loop:
+                        break;
+                    } else {    // if you got a newline, then clear currentLine:
+                        currentLine = "";
+                    }
+                } else if (c != '\r') {  // if you got anything else but a carriage return character,
+                    currentLine += c;      // add it to the end of the currentLine
+                }
+
+
+
+                // Check to see if the client request was "GET /H" or "GET /L":
+                if (currentLine.endsWith("GET /H")) {
+                    digitalWrite(4, HIGH);               // GET /H turns the LED on
+                    activeOutput = true;
+                    lastOpenTime = millis();
+                }
+                if (currentLine.endsWith("GET /L")) {
+                    digitalWrite(4, LOW);                // GET /L turns the LED off
+                }
+            }
+        }
+        // close the connection:
+        client.stop();
+        Serial.println("client disconnected");
+    }
+}
+
+void switchOff() {
+    digitalWrite(4, LOW);
+    activeOutput = false;
+}
+
+void printWifiStatus() {
+    // print the SSID of the network you're attached to:
+    Serial.print("SSID: ");
+    Serial.println(WiFi.SSID());
+
+    // print your board's IP address:
+    IPAddress ip = WiFi.localIP();
+    Serial.print("IP Address: ");
+    Serial.println(ip);
+
+    // print the received signal strength:
+    long rssi = WiFi.RSSI();
+    Serial.print("signal strength (RSSI):");
+    Serial.print(rssi);
+    Serial.println(" dBm");
+    // print where to go in a browser:
+    Serial.print("To see this page in action, open a browser to http://");
+    Serial.println(ip);
 }
